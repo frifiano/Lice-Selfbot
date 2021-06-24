33a29778811f9f7ec30aa439abbafcae8a83ac12
frifiano <86443549+frifiano@users.noreply.github.com>
2021-06-25 00:27:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-let token = "mfa.vtsBUzhkReiYAUH3tTXEgtqbgWcWBptCJHzw9Elebk-1XJ07Mc3UNmaAH0szmLZfsHZ_XrGRtSabqF9xZqfA" // Mettez le token de votre compte discord
-let prefix = "+" // Désignez un préfixe a ajouter au selfbot
-let color = "#FF0000" // Désignez une couleur a ajouter au selfbot (format HTML ou "BLUE", "YELLOW", ...)
-let image = "https://cdn.discordapp.com/attachments/857700555324784700/857709200825909288/9k.png" // Mettez une image sur les embeds du selfbot
-let premium = "oKLLPASQDLDFS456@%%" // facultatif, entrez votre clée premium du selfbot
-let nsfw = "on" // Sélectionnez On/Off pour désactiver les commandes de visibilité d'images pornographiques
+let token = "Votre token" // Mettez le token de votre compte discord
+let prefix = "" // Désignez un préfixe a ajouter au selfbot
+let color = "" // Désignez une couleur a ajouter au selfbot (format HTML ou "BLUE", "YELLOW", ...)
+let image = "" // Mettez une image sur les embeds du selfbot
+let premium = "" // facultatif, entrez votre clée premium du selfbot
+let nsfw = "" // Sélectionnez On/Off pour désactiver les commandes de visibilité d'images pornographiques
 let mod1 = "" // Ajoutez un membre ayant la possibilité d'utiliser le SelfBot
 let mod2 = "" // Ajoutez un deuxième membre ayant la possibilité d'utiliser le SelfBot
 
